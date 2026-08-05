c7c3bd3bea4341c1f6a4651c69d2dcbc990b73f9
nodeanurag <conveytoanurag@gmail.com>
2026-08-05 21:08:50 +0530

    feat(sign-out): import button component and logoutAction

---
+++ src/features/auth/sign-out-button.tsx
@@ -0,0 +1,5 @@
+import { LogOut } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { logoutAction } from "@/server/actions/auth.actions";
+import { cn } from "@/lib/utils";
